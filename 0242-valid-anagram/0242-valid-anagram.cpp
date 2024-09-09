fdf7f40f3326a3eb46bdd12805249b0d22a3716f
P Ganesh <nagpichikaganesh@gmail.com>
2024-09-09 10:28:34 +0530

    Time: 14 ms (9.94%), Space: 8.9 MB (54.83%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,9 +3,6 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        if(s == t){
-            return true;
-        }
-        return false;
+        return s == t;
     }
 };
