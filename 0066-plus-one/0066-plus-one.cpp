fa4309bd48dac4d30af02520d0e1d7196288489c
Pichika Ganesh <nagpichikaganesh@gmail.com>
2024-10-09 01:11:53 +0530

    Time: 2 ms (43.49%), Space: 10.4 MB (81.14%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        //string num;
+        // for(int i : digits){
+        //     num.append(to_string(i));
+        // }
+        // long long after_conversion = stoll(num) + 1;
+        
+        // vector<int> vec1;
+        // while(after_conversion > 0){
+        //     int end_digit = after_conversion % 10;
+        //     vec1.push_back(end_digit);
+        //     after_conversion /= 10;
+        // }
+        // reverse(vec1.begin(), vec1.end());
+        // return vec1;
+        int size_of_digits = digits.size();
+        for(int i = size_of_digits - 1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i] += 1;
+                return digits; 
+            }
+            digits[i] = 0;
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
